d466d0ed255421c66d554dcdd5d41a7f869701d5
yasu-wp <70001850+yasu-wp@users.noreply.github.com>
2020-08-21 15:30:08 +0900

    Delete script.js

--- codebegin課題１/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('menuButton').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.getElementById('nav').classList.toggle('active');
-  })
-});
