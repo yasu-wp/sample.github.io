8055c4239a707aab684faa1e6a9bb82791b42430
yasu-wp <70001850+yasu-wp@users.noreply.github.com>
2020-08-21 15:59:53 +0900

    Initial commit.
    模写サイトアップの練習

---
+++ codebegin課題１/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('menuButton').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.getElementById('nav').classList.toggle('active');
+  })
+});
